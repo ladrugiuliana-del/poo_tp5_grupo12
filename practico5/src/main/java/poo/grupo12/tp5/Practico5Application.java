f55c6f5b4c3cca77259e6ca0e7918b0fc229b766
Giuliana Ladrú <ladrugiuliana@gmail.com>
2026-09-14 17:07:20 -0300

    Proyecto Maven Vacío
    Se crea el proyecto Maven como base para realizar el TP5.

---
+++ practico5/src/main/java/poo/grupo12/tp5/Practico5Application.java
@@ -0,0 +1,13 @@
+package poo.grupo12.tp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practico5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practico5Application.class, args);
+	}
+
+}
---
+++ practico5/src/test/java/poo/grupo12/tp5/Practico5ApplicationTests.java
@@ -0,0 +1,13 @@
+package poo.grupo12.tp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practico5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
